52571be2cffb46b0774c7187687e6cbd95479628
Leah Hill <leah@verysandwich.com>
2018-07-11 15:30:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ function happyHolidays(string) {
   return 'Happy holidays!'
 }
 function happyHolidaysTo(string){
-  var name = 'you'
+  var name = `you`
   return "Happy holidays, ${name}!";
 }
 function happyHolidaysTo(string) {
- var name = 'you'
- var holiday = 'Independence Day'
+ var name = `you`
+ var holiday = `Independence Day`
  return "Happy ${holiday}, ${name}!";
 }
 function holidayCountdown(string) {
-  var days = '20'
-  var holiday = 'Mother\'s Day'
+  var days = `20`
+  var holiday = `Mother\'s Day`
 return "It\'s ${days} days until ${holiday}";
 }
